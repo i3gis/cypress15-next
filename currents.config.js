f7fbcf5033c62552afe852207d01efcd3ab4b894
muhammad chaerul hafiz <mchaerulhafiz@gmail.com>
2026-04-14 12:37:00 +0700

    update lagi

--- currents.config.js
+++ currents.config.js
@@ -1,6 +1,2 @@
 /** @type {import('cypress-cloud/types').CurrentsConfig} */
-module.exports = {
-  // Ganti dengan URL sorry-cypress director kamu
-  cloudServiceUrl: "https://cy.yourdomain.com",
-  recordKey: "your-record-key",
-};
+module.exports = {};
--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { cloudPlugin } from "cypress-cloud/plugin";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:{{changeThisPort}}",
+    baseUrl: "http://localhost:3000",
     specPattern: "cypress/e2e/**/*.cy.{js,ts}",
     supportFile: "cypress/support/e2e.ts",
     viewportWidth: 1280,
